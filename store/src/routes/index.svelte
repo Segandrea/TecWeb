9e99d0b93b3fe704d873810f0d2ccbef554ee392
Segandrea <andreasegantini97@gmail.com>
2021-11-25 17:09:26 +0100

    added faked product to index.svelte

--- store/src/routes/index.svelte
+++ store/src/routes/index.svelte
@@ -1,47 +1,79 @@
+<script context="module">
+  import faker from "faker";
+
+  export async function load({ page, fetch, session, stuff }) {
+    let products = new Array(20);
+    products.fill({
+      id: faker.datatype.uuid(),
+      img_url: faker.image.technics(),
+      name: faker.commerce.productName(),
+      description: faker.commerce.productDescription(),
+      base_price: faker.datatype.float(),
+      daily_price: faker.datatype.float(),
+    });
+
+    return {
+      props: {
+        products,
+      },
+    };
+  }
+</script>
+
+<script>
+  export let products;
+</script>
+
 <svelte:head>
   <title>Games Room</title>
 </svelte:head>
 
 <main class="container">
   <div class="row row-cols-1 row-cols-sm-2 row-cols-md-3 row-cols-lg-4 g-4">
-    <div class="col">
-      <div class="card">
-        <img src="product-image.png" class="card-img-top" />
-        <div class="card-body">
-          <h5 class="card-title">Card Title</h5>
-          <div class="card-subtitle mb-2 fs-6">
-            <span class="bi bi-star-fill orange" />
-            <span class="bi bi-star-fill orange" />
-            <span class="bi bi-star-fill orange" />
-            <span class="bi bi-star" />
-            <span class="bi bi-star" />
+    {#each products as product}
+      <div class="col">
+        <div class="card">
+          <img src={product.img_url} class="card-img-top" />
+          <div class="card-body">
+            <h5 class="card-title">{product.name}</h5>
+            <div class="card-subtitle mb-2 fs-6">
+              <span class="bi bi-star-fill orange" />
+              <span class="bi bi-star-fill orange" />
+              <span class="bi bi-star-fill orange" />
+              <span class="bi bi-star" />
+              <span class="bi bi-star" />
+            </div>
+            <p class="card-text">
+              {product.description}
+            </p>
           </div>
-          <p class="card-text">
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-            eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
-            ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
-            aliquip ex ea commodo consequat. Duis aute irure dolor in
-            reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
-            pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
-            culpa qui officia deserunt mollit anim id est laborum.
-          </p>
-        </div>
-        <div class="card-footer">
-          <table class="table table-borderless p-0 m-0">
-            <tbody>
-              <tr class="fs-5">
-                <th class="text-start orange fw-bold">Daily</th>
-                <th class="text-end orange fw-bold">$ 5,00</th>
-              </tr>
-              <tr class="fs-6">
-                <td class="text-start text-muted">Base</td>
-                <td class="text-end text-muted"><small>$ 14,00</small></td>
-              </tr>
-            </tbody>
-          </table>
+          <div class="card-footer">
+            <table class="table table-borderless p-0 m-0">
+              <tbody>
+                <tr class="fs-5">
+                  <th class="text-start orange fw-bold">Daily</th>
+                  <th class="text-end orange fw-bold"
+                    ><i class="bi bi-currency-euro">
+                      {product.daily_price}</i
+                    ></th
+                  >
+                </tr>
+                <tr class="fs-6">
+                  <td class="text-start text-muted">Base</td>
+                  <td class="text-end text-muted"
+                    ><small
+                      ><i class="bi bi-currency-euro">
+                        {product.base_price}</i
+                      ></small
+                    ></td
+                  >
+                </tr>
+              </tbody>
+            </table>
+          </div>
         </div>
       </div>
-    </div>
+    {/each}
   </div>
 </main>
 
