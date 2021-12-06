aae20405525f3976ad6b48c86bf691dd647c3a21
daddinuz <daddinuz@gmail.com>
2021-12-06 17:18:04 +0100

    add vue router

--- backoffice/.eslintrc.js
+++ backoffice/.eslintrc.js
@@ -1,11 +1,14 @@
 module.exports = {
+  root: true,
   env: {
     node: true,
-    "vue/setup-compiler-macros": true,
   },
-  extends: ["eslint:recommended", "plugin:vue/vue3-recommended", "prettier"],
+  extends: ["plugin:vue/vue3-essential", "eslint:recommended", "@vue/prettier"],
+  parserOptions: {
+    ecmaVersion: 2020,
+  },
   rules: {
-    // override/add rules settings here, such as:
-    // 'vue/no-unused-vars': 'error'
+    "no-console": process.env.NODE_ENV === "production" ? "warn" : "off",
+    "no-debugger": process.env.NODE_ENV === "production" ? "warn" : "off",
   },
 };
