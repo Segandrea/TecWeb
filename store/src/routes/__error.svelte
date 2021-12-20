d739b4c69b624b8c2902c381b66f9e3c5129bbb5
daddinuz <daddinuz@gmail.com>
2021-12-20 17:10:25 +0100

    add error page

---
+++ store/src/routes/__error.svelte
@@ -0,0 +1,24 @@
+<script context="module">
+  export function load({ error, status }) {
+    return {
+      props: {
+        message: error.message,
+        status,
+      },
+    };
+  }
+</script>
+
+<script>
+  export let status;
+  export let message;
+</script>
+
+<main class="container">
+    <div class="row">
+        <div class="col text-center p-4">
+            <h2>Error {status}</h2>
+            <h4>{message}</h4>
+        </div>
+    </div>
+</main>
