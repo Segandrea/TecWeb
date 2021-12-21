8aa749ad6bcb4615a994b89a6ad1a4ac8db7ac45
daddinuz <daddinuz@gmail.com>
2021-12-21 20:12:40 +0100

    bugfix review schema

--- server/models/review.js
+++ server/models/review.js
@@ -2,10 +2,10 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const ReviewSchema = new Schema({
-  productId: { type: mongoose.ObjectId, required: true },
+  productId: { type: mongoose.ObjectId, required: true, ref: "Product" },
   username: { type: String, required: true },
   content: { type: String, required: true },
-  rating: { type: Number, min: 0, max: 5 },
+  rating: { type: Number, min: 0, max: 5, required: true },
 });
 
 const Review = mongoose.model("Review", ReviewSchema);
