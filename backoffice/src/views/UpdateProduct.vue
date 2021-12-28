02beedeca6cc223dd245d8389bb0d4f72e5ee78e
Segandrea <andreasegantini97@gmail.com>
2021-12-28 19:41:11 +0100

    add Review table in product detail to show all related reviews

--- backoffice/src/views/UpdateProduct.vue
+++ backoffice/src/views/UpdateProduct.vue
@@ -256,6 +256,24 @@ function removeImage(index) {
         </div>
       </div>
     </form>
+
+    <h3 class="mt-4 text-center">Review list</h3>
+    <table class="table table-hover mb-4">
+      <thead>
+        <tr>
+          <th scope="col">username</th>
+          <th scope="col">content</th>
+          <th scope="col">rating</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr v-for="review in product.reviews" :key="review._id">
+          <td>{{ review.username }}</td>
+          <td>{{ review.content }}</td>
+          <td>{{ review.rating }}</td>
+        </tr>
+      </tbody>
+    </table>
   </main>
 </template>
 
